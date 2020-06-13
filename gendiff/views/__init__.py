cdf1427f25c52ab2cb12a2a5fb711c05075cfe65
mascai <msumoskalenko@gmail.com>
2020-06-14 01:11:58 +0300

    PR2: fix linter errors

--- gendiff/views/__init__.py
+++ gendiff/views/__init__.py
@@ -1,6 +1,6 @@
-from gendiff.views.json_view import format as json
-from gendiff.views.plain_view import format as plain
-from gendiff.views.default_view import format as default
+from gendiff.views.json_view import format as json # noqa F401
+from gendiff.views.plain_view import format as plain # noqa F401
+from gendiff.views.default_view import format as default # noqa F401
 
 
 FORMATTERS = (DEFAULT, PLAIN, JSON) = ('default', 'plain', 'json')
